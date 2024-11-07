7ba2e4c820beb22b6563bf7836c2f35bee024ae9
abduscript <abdupondokit@gmail.com>
2024-11-07 11:20:34 +0800

    web commit

---
+++ web.js
@@ -0,0 +1,8 @@
+// alert ('SELAMAT DATANG');
+// confirm = prompt('klik oke jika anda  bukan robot');
+
+// if (confirm == 'oke') {
+//     alert('anda bukan robot');
+// } else {
+//     alert('anda adalah robot');
+// }
